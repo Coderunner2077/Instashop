6c0d669b19866d819cd384fbec256c7970eae9e7
Coderunner2077 <coderunner2077@gmail.com>
2022-08-19 01:06:36 +0200

    Fixed Footer layout

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,7 @@ import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai';
 
 const Footer = () => {
     return (
-        <div className="text-sky-700 text-center mt-5 py-7 px-2.5 font-bold flex-y gap-2.5">
+        <div className="text-sky-700 text-center mt-5 py-7 px-2.5 font-bold flex-y gap-2.5 cursor-default">
             <p>{new Date().getFullYear()} &copy; Instashop All rights reserverd</p>
             <p className="text-3xl flex gap-2.5">
                 <AiFillInstagram />
