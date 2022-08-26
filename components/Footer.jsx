610d84b2e2572ac309ba55686f695ee2fd3725d5
Coderunner2077 <coderunner2077@gmail.com>
2022-08-26 06:19:01 +0200

    Updated footer links

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai';
+import { AiOutlineGithub, AiOutlineTwitter } from 'react-icons/ai';
 
 const Footer = () => {
     return (
         <div className="text-sky-700 text-center mt-5 py-7 px-2.5 font-bold flex-y gap-2.5 cursor-default">
             <p>{new Date().getFullYear()} &copy; Instashop All rights reserverd</p>
             <p className="text-3xl flex gap-2.5">
-                <AiFillInstagram />
-                <AiOutlineTwitter />
+                <a href="https://github.com/Coderunner2077/Instashop" target="_blank"><AiOutlineGithub /></a>
+                <a href="https://twitter.com/coderunner2077" target="_blank"><AiOutlineTwitter /></a>
             </p>
         </div>
     )
