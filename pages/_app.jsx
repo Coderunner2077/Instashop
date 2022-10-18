c6428e6d3d7f124c0a380376081144025e7b2b56
Coderunner2077 <coderunner2077@gmail.com>
2022-10-19 00:22:51 +0200

    Added and set up next-auth

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,18 +3,21 @@ import { Provider as StoreProvider } from "react-redux";
 import { useRouter } from "next/router";
 import store from "../store/store";
 import { Layout } from "../components";
+import { SessionProvider } from "next-auth/react";
 
 import '../styles/index.css'
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   const router = useRouter();
 
   return (
-    <StoreProvider store={store}>
-      <Layout>
-        <Component {...pageProps} key={router.asPath} />
-      </Layout>
-    </StoreProvider>
+    <SessionProvider session={session}>
+      <StoreProvider store={store}>
+        <Layout>
+          <Component {...pageProps} key={router.asPath} />
+        </Layout>
+      </StoreProvider>
+    </SessionProvider>
   )
 }
 
