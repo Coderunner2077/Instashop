c14bb45bd2e84e356c25e285543be3f9c9ef2a0b
Coderunner2077 <coderunner2077@gmail.com>
2022-11-02 23:49:03 +0100

    Added contact us link to success page

--- pages/success.js
+++ pages/success.js
@@ -3,6 +3,9 @@ import Link from 'next/link';
 import { BsBagCheckFill } from 'react-icons/bs';
 import { useDispatch } from "react-redux";
 import { emptyCart } from '../store/actions';
+import { showModal } from "../store/actions";
+import { ContactUs } from "../components";
+import { ErrorContext } from "../context";
 
 import { runFireworks } from '../lib/utils.js';
 
@@ -14,6 +17,10 @@ const Success = () => {
         runFireworks();
     }, []);
 
+    const handleContact = () => {
+        dispatch(showModal({ title: "Contact us", body: <ErrorContext errors={["name", "email", "message"]}><ContactUs /></ErrorContext> }));
+    }
+
     return (
         <div className="min-h-[69vh] md:min-h-[60vh] bg-white">
             <div className="flex-y gap-3 w-full sm:w-3/5 bg-gray-500 rounded-2xl p-10 mx-auto mt-10">
@@ -23,10 +30,10 @@ const Success = () => {
                 <h2 className="text-4xl text-blue-900 font-extrabold">Thank you for your order!</h2>
                 <p className="text-black text-xs">Check your email inbox for the receipt.</p>
                 <p className="text-lg">
-                    If you have any questions, please email
-                    <a className="text-red-600 ml-2" href="mailto:coderunner2077@gmail.com">
-                        coderunner2077@gmail.com
-                    </a>
+                    If you have any questions, please
+                    <button className="text-red-600 ml-2" onClick={handleContact}>
+                        contact us
+                    </button>
                 </p>
                 <Link href="/">
                     <button type="button" width="300px" className="btn btn-red mt-3">
